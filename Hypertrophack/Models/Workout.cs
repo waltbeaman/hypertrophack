e680546895d0163759e14a1be7f06445db2ddfdc
Walt Beaman <walter.g.beaman@outlook.com>
2022-11-02 12:49:26 -0500

    Removed unused models

--- Hypertrophack/Models/Workout.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hypertrophack.Models
-{
-    public class Workout
-    {
-        public int Id { get; set; }
-        public List<CompletedExercise> CompletedExercises { get; set; } = null!;
-    }
-}
